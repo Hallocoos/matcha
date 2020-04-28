65faa0a62f5d30217176550e5c6838c4e42fd0b5
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2020-04-28 15:52:57 +0200

    changed the message

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # setup.sh - Use this to install packages, setup conf.env and see if you can connect to the matcha database
-# Run `sh src/setup/setup.sh` where the `.git` file is stored.
+# Run `sh install.sh` where the `.git` file is stored.
 
 echo "I suggest using pure Windows, Ubuntu or Max. None of the WSL or WSL 2 bullshit."
 echo "WSL 1/2 is slower than pure Windows, so there's no reason to use it for Web Development if it's almost just as easy to setup."
