283c74c92231f523aaffecfa7c62a9d0fee06432
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2020-06-21 17:31:24 +0200

    done some stuff

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,9 +1,10 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
-import { modifyUserById, retrieveUserByUsername, User } from '../models/userModel';
-import { updateUserValidator } from '../services/validation';
+import { updateUserValidator, newNotificationValidator, newMatchValidator, idValidator } from '../services/validation';
+import { modifyUserById, retrieveUserByUsername, retrieveUserById } from '../models/userModel';
+import { addMatch, retrieveMatchByIds, retrieveMatchesById } from '../models/matchModel';
 import { retrieveImagesByUserId } from '../models/imageModel';
-import { retrieveNotificationsByReceiveId, retrieveNotificationsBySendIdAndReceiveId } from '../models/notificationModel';
+import { retrieveNotificationsByReceiveId, retrieveNotificationsBySendIdAndReceiveId, addNotification } from '../models/notificationModel';
 
 const router = express.Router();
 
@@ -41,23 +42,67 @@ router.post('/getNotifications', async (request: Request, response: Response) =>
   response.send({ notifications: notifications });
 });
 
-// router.post('/createNotifications', async (request: Request, response: Response) => {
+// { "sendId": 1, "receiveId": 2, "message": "New Message!" }
+router.post('/createNotifications', async (request: Request, response: Response) => {
+  let errors = newNotificationValidator(request.body);
+  if (!errors) {
+    let sender: any = await retrieveUserById(request.body.sendId);
+    let receiver: any = await retrieveUserById(request.body.receiveId);
+    if (!sender.username && !receiver.username)
+      response.send({ text: 'The user you have tried to match with does not exist.', success: false });
+    else {
+      request.body.sender = sender.username;
+      request.body.receiver = receiver.username;
+      await addNotification(request.body);
+      response.send({ text: 'The recipient will be notified.', success: true });
+    }
+  } else
+    response.send({ text: errors, success: false });
+});
+
+// { "acceptId": 5, "requestId": 3 }
+router.post('/createMatch', async (request: Request, response: Response) => {
+  let errors = newMatchValidator(request.body);
+  if (!errors) {
+    let accepter: any = await retrieveUserById(request.body.acceptId);
+    let requester: any = await retrieveUserById(request.body.requestId);
+    let match = await retrieveMatchByIds(accepter.id, requester.id);
+    if (!accepter.username && !requester.username)
+      response.send({ text: 'The user you have tried to match with does not exist.', success: false });
+    else if (match)
+      response.send({ text: 'Users are already matched.', success: false });
+    else {
+      request.body.accepter = accepter.username;
+      request.body.requester = requester.username;
+      await addMatch(request.body);
+      response.send({ text: 'The recipient will be notified.', success: true });
+    }
+  } else
+    response.send({ text: errors, success: false });
+});
+
+// { "id": 1}
+router.post('/getMatches', async (request: Request, response: Response) => {
+  let errors = idValidator(request.body.id);
+  if (!errors) {
+    let matches = await retrieveMatchesById(request.body.id);
+    response.send({ matches: matches });
+  } else
+    response.send({ text: 'Id is Invalid.', success: false });
+});
+
+// {userId: 1, image: <base64 string>"nhvf4qnhnhvqvfqnuhqwevfnuh"}
+router.post('/setProfilePicture', async (request: Request, response: Response) => {
+  // createImage();
+  // setImageAsProfilePicture();
+  response.send({ text: '', success: true});
+});
+
+// router.post('/uploadPicture', async (request: Request, response: Response) => {
 //   response.send({ text: '', success: true});
 // });
 
-// router.post('/createMatch', (request: Request, response: Response) => {
-//   response.send({ text: '', success: true});
-// });
-
-// router.post('/getMatches', (request: Request, response: Response) => {
-//   response.send({ text: '', success: true});
-// });
-
-// router.post('/getSuggestions', (request: Request, response: Response) => {
-//   response.send({ text: '', success: true});
-// });
-
-// router.post('/setProfilePicture', (request: Request, response: Response) => {
+// router.post('/getSuggestions', async (request: Request, response: Response) => {
 //   response.send({ text: '', success: true});
 // });
 
